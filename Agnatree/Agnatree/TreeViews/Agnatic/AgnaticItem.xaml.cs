6579f3151f966dbc9ea60b8c84c1573b1c73b2eb
seb776 <sebastien.maire@epitech.eu>
2015-03-29 16:39:37 +0200

    Calculating the angle for the text inside AgnaticItem

--- Agnatree/Agnatree/TreeViews/Agnatic/AgnaticItem.xaml.cs
+++ Agnatree/Agnatree/TreeViews/Agnatic/AgnaticItem.xaml.cs
@@ -24,14 +24,32 @@ namespace Agnatree.TreeViews.Agnatic
         public String Position;
         public AgnaticItem()
         {
+            Position = "110000";
             InitializeComponent();
             //MyAngle = new RotateTransform( 42 );
-            this.Text.Angle = 42;
+            this.Text.Angle = -42;
             this.Polys.Points.Clear();
             Point p = new Point( 150, 150 );
             this.Polys.Points.Add( new Point( p.X + -50, p.Y + 0 ) );
             this.Polys.Points.Add( new Point( p.X + 0, p.Y - 50 ) );
             this.Polys.Points.Add( new Point( p.X + 50, p.Y + 0 ) );
+            CalcAngle();
+        }
+
+        public void CalcAngle()
+        {
+            double _base = 45;
+            double angle = 0;
+            foreach (char c in Position)
+            {
+                if (c == '1')
+                    angle = angle - _base;
+                else
+                    angle = angle + _base;
+
+                _base = _base / 2;
+            }
+            this.Text.Angle = angle;
         }
     }
 }
