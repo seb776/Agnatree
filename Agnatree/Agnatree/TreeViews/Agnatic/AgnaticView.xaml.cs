bf40a4b24502da3253d0987beb2d43f636896d84
seb776 <sebastien.maire@epitech.eu>
2015-10-19 11:33:27 +0100

    Begining of tree + detection between circles to be able to resize + fixes for text

--- Agnatree/Agnatree/TreeViews/Agnatic/AgnaticView.xaml.cs
+++ Agnatree/Agnatree/TreeViews/Agnatic/AgnaticView.xaml.cs
@@ -20,33 +20,58 @@ namespace Agnatree.TreeViews.Agnatic
     /// </summary>
     public partial class AgnaticView : UserControl
     {
+        public List<Double> Sizes;
+        private AgnaticItem _baseItem;
         public AgnaticView()
         {
             InitializeComponent();
-            AddChild( "0" );
+            Sizes = new List<double>();
+            _baseItem = AddChild( "0" );
         }
-        public void AddChild(String position)
+        public AgnaticItem AddChild(String position)
         {
-            AgnaticItem item = new AgnaticItem();
+            AgnaticItem item = new AgnaticItem(this);
+            var backItem = item;
 
             item.PositionID = position;
             item.CalcAngle();
             item.CalcPoints();
             this.Grid.Children.Add( item );
-            item = new AddAgnaticItem();
+            item = new AddAgnaticItem(this);
             item.PositionID = position + "0";
             item.CalcAngle();
             item.CalcPoints();
             this.Grid.Children.Add(item);
-            item = new AddAgnaticItem();
+            item = new AddAgnaticItem(this);
             item.PositionID = position + "1";
             item.CalcAngle();
             item.CalcPoints();
             this.Grid.Children.Add( item );
+            return backItem;
         }
         void RemoveChild(String position)
         {
             throw new System.Exception("Not implemented: AgnaticView.RemoveChild()");
+        }
+
+        private void Grid_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Point baseView = new Point(_baseItem.Margin.Left, _baseItem.Margin.Top);
+            double dist = (baseView - e.GetPosition(this.Grid)).Length;
+            double radius = 0;
+            foreach (double s in Sizes)
+            {
+                radius += s;
+                if (Math.Abs(dist - radius) < 1)
+                {
+                    Mouse.OverrideCursor = Cursors.ScrollNS;
+                    break;
+                }
+                else
+                {
+                    Mouse.OverrideCursor = null;
+                }
+            }
         }
     }
 }
